43502f87e7b8541828c4bf22182f77ef5e2e6728
samarhussain007 <admin.development@gameduels.in>
2024-01-21 00:35:53 +0530

    created Modal Provider for gloabalising modals

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,17 +1,23 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { Modal } from "@/components/ui/modal";
+import { useEffect } from "react";
 import { UserButton } from "@clerk/nextjs";
 
+import { Button } from "@/components/ui/button";
+import { Modal } from "@/components/ui/modal";
+import { useStoreModal } from "@/hooks/use-store-modal";
+
 const SetupPage = () => {
-  return (
-    <div className="p-4">
-      <Modal title="Test" description="hellow" isOpen onClose={() => {}}>
-        children
-      </Modal>
-    </div>
-  );
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
+
+  return <div className="p-4"></div>;
 };
 
 export default SetupPage;
---
+++ components/modals/store-modal.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { Modal } from "@/components/ui/modal";
+import { useStoreModal } from "@/hooks/use-store-modal";
+
+export const StoreModal = () => {
+  const storeModal = useStoreModal();
+  return (
+    <Modal
+      title="Create Store"
+      description="Add new Store to manage products and categories"
+      isOpen={storeModal.isOpen}
+      onClose={storeModal.onClose}
+    >
+      Future Create Store Form
+    </Modal>
+  );
+};
